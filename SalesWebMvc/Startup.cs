38c2ab17db9fa5298172b096c5570e7a2cc67318
Davidson Dias <davidsonsd16@gmail.com>
2019-11-20 21:38:40 -0300

    Seeding Service, SellersController and SellerService

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -10,30 +10,43 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
-namespace SalesWebMvc {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace SalesWebMvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
             services.AddControllersWithViews();
 
-    services.AddDbContext<SalesWebMvcContext>(options =>
-            options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
-                builder.MigrationsAssembly("SalesWebMvc")));
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvc")));
+
+            services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
-            else {
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
